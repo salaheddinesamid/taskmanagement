fa1d811f6a846bb994c6f740dc1bc928b8d30d6a
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-12-12 17:35:18 +0100

    add task dto to handle task assignment

--- src/main/java/com/taskflow/service4/dto/TaskAssignmentDTO.java
+++ src/main/java/com/taskflow/service4/dto/TaskAssignmentDTO.java
@@ -1,2 +1,18 @@
-package com.taskflow.service4.dto;public class TaskAssignmentDTO {
+package com.taskflow.service4.dto;
+
+import lombok.Data;
+
+@Data
+public class TaskAssignmentDTO {
+
+    Integer taskId;
+    Integer userId;
+
+    public TaskAssignmentDTO(
+            Integer taskId,
+            Integer userId
+    ){
+        this.taskId = taskId;
+        this.userId = userId;
+    }
 }
