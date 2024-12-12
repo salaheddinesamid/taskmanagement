a0fefea5e07377aaad9892f20b54991f7593e080
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-12-12 17:41:27 +0100

    add new method

--- src/main/java/com/taskflow/service4/repository/TaskRepository.java
+++ src/main/java/com/taskflow/service4/repository/TaskRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Integer> {
     List<Task> findAllByProjectId(Integer projectId);
+    List<Task> findAllByAssignedToUserId(Integer userId);
 }
