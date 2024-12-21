3edfb922c15e5d5c48400a8621ce1a61e69bed24
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-12-21 16:37:55 +0100

    add filter

---
+++ src/main/java/com/taskflow/service4/configuration/RequestFilter.java
@@ -0,0 +1,24 @@
+package com.taskflow.service4.configuration;
+
+import jakarta.servlet.*;
+
+import javax.xml.transform.Source;
+import java.io.IOException;
+
+public class RequestFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Request from:" + servletRequest.getRemoteAddr());
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
